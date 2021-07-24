80a9b20c4c5aff753e34b8d25cbb63852e17a880
little-pinecone <marta.szymek.mail@gmail.com>
2021-07-24 17:57:25 +0200

    Initial commit

---
+++ src/main/java/in/keepgrowing/springbootswaggeruibasicauth/SpringBootSwaggerUiBasicAuthApplication.java
@@ -0,0 +1,13 @@
+package in.keepgrowing.springbootswaggeruibasicauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSwaggerUiBasicAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSwaggerUiBasicAuthApplication.class, args);
+	}
+
+}
