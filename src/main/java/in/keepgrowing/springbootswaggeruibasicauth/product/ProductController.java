4237c714bb040619ba4099c17d8db5c92237af34
little-pinecone <marta.szymek.mail@gmail.com>
2022-03-07 14:20:46 +0100

    feat: configure csrf protection
    * enable csrf in Springdoc
    * update SecurityConfig
    * create a DELETE endpoint to verify csrf setup

--- src/main/java/in/keepgrowing/springbootswaggeruibasicauth/product/ProductController.java
+++ src/main/java/in/keepgrowing/springbootswaggeruibasicauth/product/ProductController.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,11 +23,17 @@ public class ProductController {
         this.productRepository = productRepository;
     }
 
-    @GetMapping()
+    @GetMapping
     @Operation(summary = "Find all products")
     public ResponseEntity<List<ProductDto>> findAll() {
         var products = productRepository.findAll();
 
         return new ResponseEntity<>(products, HttpStatus.OK);
+    }
+
+    @DeleteMapping
+    @Operation(summary = "Endpoint for testing CSRF configuration, doesn't remove anything")
+    public ResponseEntity<Void> delete() {
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/in/keepgrowing/springbootswaggeruibasicauth/shared/config/security/SecurityConfig.java
+++ src/main/java/in/keepgrowing/springbootswaggeruibasicauth/shared/config/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package in.keepgrowing.springbootswaggeruibasicauth.shared.config.security;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -16,6 +17,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .and()
                 .cors()
                 .and()
                 .authorizeRequests()
